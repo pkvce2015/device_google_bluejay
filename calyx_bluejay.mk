9eeaeb2f7cf57ad55b9856524a4797c3a14edbb1
Michael Bestas <mkbestas@gmail.com>
2022-12-07 05:25:18 +0200

    Update fingerprint/build description from TQ1A.221205.011
    Change-Id: I7c9edf6bcd45f3e3988588e59895979e82bfd4fc

--- calyx_bluejay.mk
+++ calyx_bluejay.mk
@@ -13,9 +13,9 @@ PRODUCT_BRAND := google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=bluejay \
-    PRIVATE_BUILD_DESC="bluejay-user 13 TP1A.221105.002 9080065 release-keys"
+    PRIVATE_BUILD_DESC="bluejay-user 13 TQ1A.221205.011 9244662 release-keys"
 
-BUILD_FINGERPRINT := google/bluejay/bluejay:13/TP1A.221105.002/9080065:user/release-keys
+BUILD_FINGERPRINT := google/bluejay/bluejay:13/TQ1A.221205.011/9244662:user/release-keys
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
