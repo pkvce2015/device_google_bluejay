207a2c5687bfa3b85c5e90924961333191686f6a
Chirayu Desai <chirayudesai1@gmail.com>
2023-06-14 03:03:35 +0530

    Update fingerprint/build description from TQ3A.230605.010
    Change-Id: I059cd41f818dea0f5b1aaa4d3e8a1748e0d0bb13

--- calyx_bluejay.mk
+++ calyx_bluejay.mk
@@ -13,9 +13,9 @@ PRODUCT_BRAND := google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=bluejay \
-    PRIVATE_BUILD_DESC="bluejay-user 13 TQ2A.230505.002 9891397 release-keys"
+    PRIVATE_BUILD_DESC="bluejay-user 13 TQ3A.230605.010 10121037 release-keys"
 
-BUILD_FINGERPRINT := google/bluejay/bluejay:13/TQ2A.230505.002/9891397:user/release-keys
+BUILD_FINGERPRINT := google/bluejay/bluejay:13/TQ3A.230605.010/10121037:user/release-keys
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
